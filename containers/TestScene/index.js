ab7b93d0bbd50e84de57f55c191d65ee40b33784
Pratik Singh <pratiktsingh17@gmail.com>
2020-06-06 10:53:15 +0530

    - Integrated viro-react. - Added testScene.

---
+++ containers/TestScene/index.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {ViroARScene, ViroText, ViroConstants} from 'react-viro';
+
+class TestScene extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: 'Initializing AR...',
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state === ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: 'Hello World!',
+      });
+    } else if (state === ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+export default TestScene;
